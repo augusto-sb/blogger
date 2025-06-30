034c32be452ee9a774c89b6b56a31a4dc12393c3
asaldivar <augustosaldivar@assertsolutions.com>
2025-06-30 16:06:29 -0300

    kc image: add time comparison

--- keycloak_image/steps.sh
+++ keycloak_image/steps.sh
@@ -12,3 +12,12 @@ docker container run --rm chustos.io/keycloak/stripped:26.2.5;
 #docker pull container-registry.oracle.com/graalvm/jdk:21
 
 # docker image pull quay.io/quarkus/ubi9-quarkus-graalvmce-builder-image:jdk-21
+
+docker image build . -t chustos.io/keycloak/stripped:26.2.5-dev -f Dockerfile.dev;
+
+
+
+time docker container run --rm quay.io/keycloak/keycloak:26.2.5-0 start
+#real	0m6,495s
+time docker run --rm chustos.io/keycloak/stripped:26.2.5-dev start
+#real	0m0,657s
