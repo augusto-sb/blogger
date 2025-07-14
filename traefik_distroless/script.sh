a53a51a0fb87bf2f717abec81ee51e47d6333cc4
asaldivar <augustosaldivar@assertsolutions.com>
2025-07-14 10:41:18 -0300

    traefik

--- traefik_distroless/script.sh
+++ traefik_distroless/script.sh
@@ -1,1 +1,3 @@
-docker image build . --tag chustos.io/static/traefik:3.4.4 --file Dockerfile --platform linux/amd64
+docker image build . --tag chustos.io/static/traefik:3.4.4 --file Dockerfile --platform linux/amd64;
+
+#https://doc.traefik.io/traefik/getting-started/install-traefik/
